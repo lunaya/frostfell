b18167474e0061e43e2faa91156e497f0fcc5ee7
lunaya <jyajjimento@gmail.com>
2017-03-13 10:41:22 -0700

    commit to pull

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
 const axios = require('axios')
 const client = new Discord.Client()
-const TwitchList = require('./twitchlist.js')
-const twitchList = TwitchList
+// const TwitchList = require('./twitchlist.js')
+// const twitchList = TwitchList
 
 client.on('ready', () => {
   console.log('I am ready!')
   // twitchPoller()
+
 });
 
 // (function twitchPoller(){
@@ -59,7 +60,9 @@ client.on('message', message => {
   var firstWord = msgArray[0]
 
   if (firstWord === '/test') {
-    message.guild.channels.find("loop", "#frostfell_no").sendMessage("nyan");
+    // message.guild.channels.find("loop", "#frostfell_no").sendMessage("nyan");
+    var channel = "278302846926192651"
+    channel.send('beep')
   }
 
   if (firstWord === '/halp') {
@@ -81,144 +84,142 @@ client.on('message', message => {
       message.channel.sendMessage(':raised_hands:')
   }
 
-  if (firstWord === '/gif') {
-    msgArray.shift()
-    msgArray.join('-')
-    const searchTerms = regex(msgArray, "-")
-    // console.log(searchTerms)
+  //85-217
+  // if (firstWord === '/gif') {
+  //   msgArray.shift()
+  //   msgArray.join('-')
+  //   const searchTerms = regex(msgArray, "-")
 
-    const getGfyLink = axios({
-      method: 'get',
-      url: process.env.gfyGetUrl + searchTerms,
-      headers: {
-        "Authorization": "Bearer " + process.env.gfyKey,
-      }
-    })
-    .then(function(response){
-      if (!response.data.gfycats) {
-        message.channel.sendMessage("I couldn't find that! D:")
-      }
-      else{
-        const gfyCatId = response.data.gfycats[randomArrayIndex(response.data.gfycats)].gfyId
-        message.channel.sendMessage("https://gfycat.com/" + gfyCatId)
-      }
-    })
-    .catch(function(response){
-      // console.log("gfycat error, running key getter")
+  //   const getGfyLink = axios({
+  //     method: 'get',
+  //     url: process.env.gfyGetUrl + searchTerms,
+  //     headers: {
+  //       "Authorization": "Bearer " + process.env.gfyKey,
+  //     }
+  //   })
+  //   .then(function(response){
+  //     if (!response.data.gfycats) {
+  //       message.channel.sendMessage("I couldn't find that! D:")
+  //     }
+  //     else{
+  //       const gfyCatId = response.data.gfycats[randomArrayIndex(response.data.gfycats)].gfyId
+  //       message.channel.sendMessage("https://gfycat.com/" + gfyCatId)
+  //     }
+  //   })
+  //   .catch(function(response){
 
-      const getKey = axios({
-        method: 'post',
-        url: process.env.gfyAuthUrl,
-        data: {
-          "grant_type": "client_credentials",
-          "client_id": process.env.gfyClientId,
-          "client_secret": process.env.gfyClientSecret
-        }
-      })
-      .then(function(response){
+  //     const getKey = axios({
+  //       method: 'post',
+  //       url: process.env.gfyAuthUrl,
+  //       data: {
+  //         "grant_type": "client_credentials",
+  //         "client_id": process.env.gfyClientId,
+  //         "client_secret": process.env.gfyClientSecret
+  //       }
+  //     })
+  //     .then(function(response){
 
-        const gfyKey = response.data.access_token
-        process.env.gfyKey = gfyKey
+  //       const gfyKey = response.data.access_token
+  //       process.env.gfyKey = gfyKey
 
-        const getGfyLink = axios({
-          method: 'get',
-          url: process.env.gfyGetUrl + searchTerms,
-          headers: {
-            "Authorization": "Bearer " + process.env.gfyKey,
-          }
-        })
-        .then(function(response){
-          if (!response.data.gfycats) {
-            message.channel.sendMessage("I couldn't find that! D:")
-          }
-          else{
-            const gfyCatId = response.data.gfycats[randomArrayIndex(response.data.gfycats)].gfyId
-            message.channel.sendMessage("https://gfycat.com/" + gfyCatId)
-          }
-        })
-      })
-    })
-  }
+  //       const getGfyLink = axios({
+  //         method: 'get',
+  //         url: process.env.gfyGetUrl + searchTerms,
+  //         headers: {
+  //           "Authorization": "Bearer " + process.env.gfyKey,
+  //         }
+  //       })
+  //       .then(function(response){
+  //         if (!response.data.gfycats) {
+  //           message.channel.sendMessage("I couldn't find that! D:")
+  //         }
+  //         else{
+  //           const gfyCatId = response.data.gfycats[randomArrayIndex(response.data.gfycats)].gfyId
+  //           message.channel.sendMessage("https://gfycat.com/" + gfyCatId)
+  //         }
+  //       })
+  //     })
+  //   })
+  // }
 
-  if (firstWord === '/rng') {
-    if (msgArray[1]){
-      if (isNormalInteger(msgArray[1]) === false){
-        message.channel.sendMessage("I can't roll this D:")
-      }
-      else {
-        message.channel.sendMessage('ROLL ' + msgArray[1] + '! -> ' + roller(msgArray[1]))
-      }      
-    }
-    else {
-      message.channel.sendMessage('ROLL 100! -> ' + roller(100))
-    }
-  }
+  // if (firstWord === '/rng') {
+  //   if (msgArray[1]){
+  //     if (isNormalInteger(msgArray[1]) === false){
+  //       message.channel.sendMessage("I can't roll this D:")
+  //     }
+  //     else {
+  //       message.channel.sendMessage('ROLL ' + msgArray[1] + '! -> ' + roller(msgArray[1]))
+  //     }      
+  //   }
+  //   else {
+  //     message.channel.sendMessage('ROLL 100! -> ' + roller(100))
+  //   }
+  // }
 
-  if (firstWord === '/d') {
-    if (!msgArray[1]){
-      message.channel.sendMessage('Rolling 1d20 -> ' + roller(20))
-    }
-    else {
-      if (isNormalInteger(msgArray[1]) === false){
-        message.channel.sendMessage("I can't roll this D:")
-      }
-      else {
-        if (!msgArray[2]){
-          message.channel.sendMessage(dRoll(msgArray[1], 1))
-        }
-        else {
-          if (isNormalInteger(msgArray[2]) === false){
-            message.channel.sendMessage("I can't roll this D:")
-          }
-          else {
-            message.channel.sendMessage(dRoll(msgArray[1], msgArray[2]))
-          }
-        }
-      }
-    }
-  }
+  // if (firstWord === '/d') {
+  //   if (!msgArray[1]){
+  //     message.channel.sendMessage('Rolling 1d20 -> ' + roller(20))
+  //   }
+  //   else {
+  //     if (isNormalInteger(msgArray[1]) === false){
+  //       message.channel.sendMessage("I can't roll this D:")
+  //     }
+  //     else {
+  //       if (!msgArray[2]){
+  //         message.channel.sendMessage(dRoll(msgArray[1], 1))
+  //       }
+  //       else {
+  //         if (isNormalInteger(msgArray[2]) === false){
+  //           message.channel.sendMessage("I can't roll this D:")
+  //         }
+  //         else {
+  //           message.channel.sendMessage(dRoll(msgArray[1], msgArray[2]))
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
-  if (firstWord === '/lewd') {
-    if (!msgArray[1]) {
-      const searchTerms = ""
+  // if (firstWord === '/lewd') {
+  //   if (!msgArray[1]) {
+  //     const searchTerms = ""
 
-      const getLewds = axios({
-        method: 'get',
-        url: process.env.booruGetUrl + searchTerms,
-      })
-      .then(function(response){
-        const booruId = response.data[randomArrayIndex(response.data)].id
-        message.channel.sendMessage(process.env.booruPostUrl + booruId)
-      })
-      .catch(function(response){
-        // console.log(response)
-      })
-    }
-    else {
-      msgArray.shift()
-      const searchTerms = msgArray.join('+')
+  //     const getLewds = axios({
+  //       method: 'get',
+  //       url: process.env.booruGetUrl + searchTerms,
+  //     })
+  //     .then(function(response){
+  //       const booruId = response.data[randomArrayIndex(response.data)].id
+  //       message.channel.sendMessage(process.env.booruPostUrl + booruId)
+  //     })
+  //     .catch(function(response){
+  //     })
+  //   }
+  //   else {
+  //     msgArray.shift()
+  //     const searchTerms = msgArray.join('+')
 
-      console.log(searchTerms)
+  //     console.log(searchTerms)
       
-      const getLewds = axios({
-        method: 'get',
-        url: process.env.booruGetUrl + searchTerms,
-      })
-      .then(function(response){
-        if (response.data.length == 0){
-          message.channel.sendMessage("This tag doesn't exist D:a")
-        }
-        else{
-          const booruId = response.data[randomArrayIndex(response.data)].id
-          message.channel.sendMessage(process.env.booruPostUrl + booruId)
-        }
-      })
-      .catch(function(response){
-        // console.log(response)
-      })
-    }
-  }
+  //     const getLewds = axios({
+  //       method: 'get',
+  //       url: process.env.booruGetUrl + searchTerms,
+  //     })
+  //     .then(function(response){
+  //       if (response.data.length == 0){
+  //         message.channel.sendMessage("This tag doesn't exist D:a")
+  //       }
+  //       else{
+  //         const booruId = response.data[randomArrayIndex(response.data)].id
+  //         message.channel.sendMessage(process.env.booruPostUrl + booruId)
+  //       }
+  //     })
+  //     .catch(function(response){
+  //     })
+  //   }
+  // }
 });
 
-const login = process.env.loginKey
+// const login = process.env.loginKey
+const login = "Mjc4MjcwMTE4MjQzMzM2MTkz.C6SSZA.cq2HZlI8rd2jdnlvoEumyADeX88"
 client.login(login)
